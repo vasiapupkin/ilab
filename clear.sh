4342d285718fc8f70a446237233d476f9806cba2
Thar0l <gusarov.aa@gmail.com>
2014-09-02 15:15:17 +0400

    Update clear.sh

--- clear.sh
+++ clear.sh
@@ -1,10 +1,15 @@
 #!/bin/bash 
+
 SRCDIR="src"
 OBJDIR="obj"
 BINDIR="bin"
 MAKEFILE="makefile"
-rm -rf $SRCDIR $OBJDIR $BINDIR
-if [ -d $SRCDIR ]; then rm -rf $SRCDIR
-if [ -d $OBJDIR ]; then rm -rf $OBJDIR
-if [ -d $BINDIR ]; then rm -rf $BINDIR
-if [ -e $MAKEFILE ]; then rm $MAKEFILE
+
+if [ -d $SRCDIR ]; then 	rm -rf $SRCDIR 
+fi
+if [ -d $OBJDIR ]; then 	rm -rf $OBJDIR 
+fi
+if [ -d $BINDIR ]; then 	rm -rf $BINDIR 
+fi
+if [ -e $MAKEFILE ]; then	rm $MAKEFILE 
+fi
